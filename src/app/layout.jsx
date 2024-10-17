48f5a06dd5ef37d6a4bb3687eb40948cf49b9420
Ankit Upadhyay <ankit.upa007@gmail.com>
2024-10-18 00:49:16 +0530

    og image, favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,6 +16,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        {/* <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-8G1MPQ72YR`}
+          strategy="afterInteractive"
+          as="script"
+          rel="preload"
+        />
+        <script async id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8G1MPQ72YR');
+        `}}>
+        </script> */}
+        <meta content="text/html; charset=UTF-8" name="Content-Type" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        <meta name="language" content="en-us" />
+        <meta name="theme-color" content="#B4D6CD" />
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
+      </head>
       <body
         className={myFont.className}
       >
